83c1867d6d909a357c60bad36a0045a8e7475f24
SHANTHAKUMAR <shanthakmr6@gmail.com>
2020-07-29 15:31:17 +0530

    Code cleanup

--- src/main/java/com/rabobank/lms/controllers/AuthController.java
+++ src/main/java/com/rabobank/lms/controllers/AuthController.java
@@ -50,7 +50,7 @@ public class AuthController {
 	JwtUtils jwtUtils; 
 
 	@PostMapping("/signin")
-	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+	public ResponseEntity<JwtResponse> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 		
 		logger.info("Entered authenticate user Method for user :{}", loginRequest.getUsername());
 
