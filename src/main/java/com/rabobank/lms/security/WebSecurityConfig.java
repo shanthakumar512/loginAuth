d74004e11a9fba720853111f6c74d406cd8f1c1d
SHANTHAKUMAR <shanthakmr6@gmail.com>
2020-08-11 19:55:45 +0530

    Swagger config

--- src/main/java/com/rabobank/lms/security/WebSecurityConfig.java
+++ src/main/java/com/rabobank/lms/security/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -33,6 +34,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private AuthEntryPointJwt unauthorizedHandler;
+	
+	
 
 	@Bean
 	public AuthTokenFilter authenticationJwtTokenFilter() {
@@ -54,6 +57,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	 @Override
+	    public void configure(WebSecurity web)  {
+	        web.ignoring().antMatchers("/v3/api-docs",
+	                "/swagger-ui.html#",
+	                "/swagger-ui/**");
+	    }
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
