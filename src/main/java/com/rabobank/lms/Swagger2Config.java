bdc8285fe5cc8a1feab271046127919e5d69cd90
unknown <ctsjava101@iiht.tech>
2020-08-12 10:07:19 +0530

    Security Config changes

--- src/main/java/com/rabobank/lms/Swagger2Config.java
+++ src/main/java/com/rabobank/lms/Swagger2Config.java
@@ -17,7 +17,7 @@ public class Swagger2Config {
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.basePackage("com.rabobank.lms.controller"))              
+          .apis(RequestHandlerSelectors.basePackage("com.rabobank.lms.controllers"))              
           .paths(PathSelectors.any())                          
           .build();                                           
     }
